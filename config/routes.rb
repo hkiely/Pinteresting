dfa13023d88377261e15e10be22618eb0da65f5b
Hunter Kiely <Hunter.kiely@gmail.com>
2013-10-29 12:53:09 -0500

    Added bootstrap grid system to team page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about"
+  get "about" => "pages#about" # creates about_path, first part of line
+  get "team" => "pages#team" #creates team_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
